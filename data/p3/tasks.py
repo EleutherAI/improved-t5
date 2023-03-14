87d3a9354cea4221c444682a3fd20edbee7957f5
lintangsutawika <lintang@sutawika.com>
2023-03-14 14:25:49 +0000

    enables importing from original t0 and re-adding with custom vocab

--- data/p3/tasks.py
+++ data/p3/tasks.py
@@ -13,103 +13,27 @@ For more details, see: seqio/scripts/cache_tasks_main.py
 """
 
 import seqio
+import t0.seqio_tasks as t0_tasks
 
 from data.vocab import DEFAULT_OUTPUT_FEATURES
-
-from data.p3 import p3_utils
 
 TaskRegistry = seqio.TaskRegistry
 MixtureRegistry = seqio.MixtureRegistry
 
 # ==================================== P3 ======================================
-# Adapted from T-Zero
+# Re-add Tasks to use with different vocab in DEFAULT_OUTPUT_FEATURES
 
-# 3 stages of training/ablation: D4 -> GPT -> SuperGLUE
-t0_train_mixture = {key: [] for key in p3_utils.t0_train}
-t0_eval_mixture = {key: [] for key in p3_utils.t0_eval}
-mixture_cap = {}
-for dataset_name, subset_name in p3_utils.all_templates.keys:
-    if (dataset_name, subset_name) not in p3_utils.all_datasets:
-        p3_utils.all_templates.remove(dataset_name, subset_name)
-        continue
+all_task_names = list(TaskRegistry.names()).copy()
 
-    cap = p3_utils.get_cap(dataset_name, subset_name)
-    dataset = p3_utils.all_templates.get_dataset(dataset_name, subset_name)
+for task_name in all_task_names:
 
-    for template_name in dataset.all_template_names:
-        # Add train and normal eval tasks
-        template = p3_utils.all_templates.get_dataset(dataset_name, subset_name)[template_name]
-        task_name = "p3_"+p3_utils.get_task_name(dataset_name, subset_name, template_name)
-        TaskRegistry.add(
-            name=task_name,
-            source=p3_utils.get_p3_source(dataset_name, subset_name, template_name),
-            preprocessors=[
-                seqio.preprocessors.tokenize,
-                seqio.preprocessors.append_eos,
-                seqio.CacheDatasetPlaceholder(required=False),
-            ],
-            postprocess_fn=p3_utils.maybe_get_class_id_postprocessor(template),
-            output_features=DEFAULT_OUTPUT_FEATURES,
-            metric_fns=p3_utils.get_p3_metric(dataset_name, subset_name, template_name)
-        )
-
-        # # Add rank classification eval task
-        # if template.answer_choices:
-        #     rank_classification_preprocessor = functools.partial(
-        #         t5.data.preprocessors.rank_classification,
-        #         inputs_fn=lambda ex: tf.fill((len(ex["answer_choices"]),), ex["inputs"]),
-        #         targets_fn=lambda ex: ex["answer_choices"],
-        #         is_correct_fn=lambda ex: tf.equal(ex["answer_choices"], tf.strings.strip(ex["targets"])),
-        #         weight_fn=lambda ex: 1.0,
-        #     )
-        #     fixed_choices = template.get_fixed_answer_choices_list()
-        #     num_classes = len(fixed_choices) if fixed_choices else None
-        #     seqio.TaskRegistry.add(
-        #         task_name + "_score_eval",
-        #         data_source,
-        #         preprocessors=[rank_classification_preprocessor] + preprocessors,
-        #         output_features=output_features,
-        #         metric_fns=[functools.partial(t5.evaluation.metrics.rank_classification, num_classes=num_classes)],
-        #         postprocess_fn=t5.data.postprocessors.rank_classification,
-        #     )
-
-        # Check that the dataset_subset_tuple is in t0_train
-        for key, dataset_subset_tuples in p3_utils.t0_train.items():
-            if (dataset_name, subset_name) in dataset_subset_tuples:
-                t0_train_mixture[key].append(task_name)
-
-        for key, dataset_subset_tuples in p3_utils.t0_eval.items():
-            if (dataset_name, subset_name) in dataset_subset_tuples:
-                t0_eval_mixture[key].append(task_name)
-
-
-MixtureRegistry.add(
-    "t0_train",
-    [task for task in t0_train_mixture["BASE"] \
-                        if task not in p3_utils.TASK_BLACKLIST],
-    default_rate=lambda t: p3_utils.mixture_cap[t.name],
-)
-
-MixtureRegistry.add(
-    "t0+_train",
-    [task for task in t0_train_mixture["BASE"] \
-                    + t0_train_mixture["GPT_EVAL"] 
-                        if task not in p3_utils.TASK_BLACKLIST],
-    default_rate=lambda t: p3_utils.mixture_cap[t.name],
-)
-
-MixtureRegistry.add(
-    "t0++_train",
-    [task for task in t0_train_mixture["BASE"] \
-                    + t0_train_mixture["GPT_EVAL"] \
-                    + t0_train_mixture["SGLUE"] \
-                        if task not in p3_utils.TASK_BLACKLIST],
-    default_rate=lambda t: p3_utils.mixture_cap[t.name],
-)
-
-MixtureRegistry.add(
-    "t0_eval",
-    [task for task in t0_eval_mixture["BASE"] \
-                        if task not in p3_utils.TASK_BLACKLIST],
-    default_rate=lambda t: p3_utils.mixture_cap[t.name],
-)
+    original_task = seqio.get_mixture_or_task(task_name)
+    TaskRegistry.remove(task_name)
+    TaskRegistry.add(
+        name=original_task.name,
+        source=original_task.source,
+        preprocessors=original_task.preprocessors,
+        postprocess_fn=original_task.postprocess_fn,
+        output_features=DEFAULT_OUTPUT_FEATURES,
+        metric_fns=original_task.metric_fns
+    )
