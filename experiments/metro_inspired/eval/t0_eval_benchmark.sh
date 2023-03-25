d1ea3a7eaebe71e38186b1bf43e24b1ae47fd9a6
lintangsutawika <lintang@sutawika.com>
2023-03-25 14:43:42 +0000

    fixes

--- experiments/metro_inspired/eval/t0_eval_benchmark.sh
+++ experiments/metro_inspired/eval/t0_eval_benchmark.sh
@@ -9,8 +9,9 @@ python -m t5x.train \
     --gin.TRAIN_STEPS=135000 \
     --gin.SAVING_PERIOD=5000 \
     --gin.MODEL_DIR=\"'/fsx/lintangsutawika/improved_t5/ckpts/METRO/benchmark_pile_mlm/finetune_t0_eval/'\" \
-    --gin.INITIAL_CHECKPOINT_PATH=\"'/fsx/lintangsutawika/improved_t5/ckpts/METRO/benchmark_pile_mlm/finetune_t0_train/'\" \
-    --gin.USE_CACHED_TASKS=False \
+    --gin.INITIAL_CHECKPOINT_PATH=\"'/fsx/lintangsutawika/improved_t5/ckpts/METRO/benchmark_pile_mlm/finetune_t0_train/checkpoint_135000'\" \
+    --seqio_additional_cache_dirs="${CACHED_DATA_DIR}" \
+    --gin.USE_CACHED_TASKS=True \
     --alsologtostderr \
     --multiprocess_gpu \
     --coordinator_address=${ADDR} \
