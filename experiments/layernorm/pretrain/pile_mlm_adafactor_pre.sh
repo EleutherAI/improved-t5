c15541511978e1c73829b5a3488165931ad456ea
lintangsutawika <lintang@sutawika.com>
2023-04-22 06:48:20 +0000

    added layernorm exp

---
+++ experiments/layernorm/pretrain/pile_mlm_adafactor_pre.sh
@@ -0,0 +1,16 @@
+ADDR=$1
+MODEL_DIR=$2
+
+python -m t5x.train \
+    --gin_file="models/scalable_t5/t5_1_1/base.gin" \
+    --gin_file="configs/task/pretrain/pile_mlm.gin" \
+    --gin_file="configs/exp/LayerNorm/pre_layernorm.gin" \
+    --gin.TRAIN_STEPS=125000 \
+    --gin.SAVING_PERIOD=25000 \
+    --gin.MODEL_DIR=\"'/fsx/lintangsutawika/improved_t5/ckpts/LayerNorm/pile_mlm_adafactor_pre/'\" \
+    --gin.USE_CACHED_TASKS=False \
+    --alsologtostderr \
+    --multiprocess_gpu \
+    --coordinator_address=${ADDR} \
+    --process_count=${SLURM_NTASKS} \
+    --process_index=${SLURM_PROCID}
---
+++ experiments/layernorm/pretrain/pile_mlm_adamw_pre.sh
@@ -0,0 +1,17 @@
+ADDR=$1
+MODEL_DIR=$2
+
+python -m t5x.train \
+    --gin_file="models/scalable_t5/t5_1_1/base.gin" \
+    --gin_file="configs/task/pretrain/pile_mlm.gin" \
+    --gin_file="configs/exp/LayerNorm/training.gin" \
+    --gin_file="configs/exp/LayerNorm/pre_layernorm.gin" \
+    --gin.TRAIN_STEPS=125000 \
+    --gin.SAVING_PERIOD=25000 \
+    --gin.MODEL_DIR=\"'/fsx/lintangsutawika/improved_t5/ckpts/LayerNorm/pile_mlm_adamw_pre/'\" \
+    --gin.USE_CACHED_TASKS=False \
+    --alsologtostderr \
+    --multiprocess_gpu \
+    --coordinator_address=${ADDR} \
+    --process_count=${SLURM_NTASKS} \
+    --process_index=${SLURM_PROCID}
