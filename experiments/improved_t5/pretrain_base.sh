efbeb5a8b0748e9927a75ce00d34da73b3eb3a99
lintangsutawika <lintang@sutawika.com>
2023-04-26 13:25:39 +0000

    final base model

--- experiments/improved_t5/pretrain_base.sh
+++ experiments/improved_t5/pretrain_base.sh
@@ -5,7 +5,7 @@ python3 -m t5x.train \
     --gin_file="configs/t5v2/base.gin" \
     --gin_file="configs/task/pretrain/pile_mized_objective.gin" \
     --gin.MIXTURE_OR_TASK_NAME="pile_ul2_causal_0_50" \
-    --gin.TASK_FEATURE_LENGTHS=\"{'inputs':2048, 'targets': 2048}\" \
+    --gin.TASK_FEATURE_LENGTHS="{'inputs':2048, 'targets': 2048}" \
     --gin.BATCH_SIZE=512 \
     --gin.TRAIN_STEPS=100000 \
     --gin.SAVING_PERIOD=5000 \
