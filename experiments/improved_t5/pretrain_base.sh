694dc33df436a66ad7d5e57347149c413f66e7ab
lintangsutawika <lintang@sutawika.com>
2023-04-26 13:32:23 +0000

    fixed typo

--- experiments/improved_t5/pretrain_base.sh
+++ experiments/improved_t5/pretrain_base.sh
@@ -3,7 +3,7 @@ MODEL_DIR=$2
 
 python3 -m t5x.train \
     --gin_file="configs/t5v2/base.gin" \
-    --gin_file="configs/task/pretrain/pile_mized_objective.gin" \
+    --gin_file="configs/task/pretrain/pile_mixed_objective.gin" \
     --gin.MIXTURE_OR_TASK_NAME="pile_ul2_causal_0_50" \
     --gin.TASK_FEATURE_LENGTHS="{'inputs':2048, 'targets': 2048}" \
     --gin.BATCH_SIZE=512 \
