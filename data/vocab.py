7a04d50b04cfee5f52ca9397b0b172d1cf887cd7
lintangsutawika <lintang@sutawika.com>
2023-04-27 15:14:28 +0000

    use custom gcs

--- data/vocab.py
+++ data/vocab.py
@@ -8,8 +8,8 @@ import tensorflow as tf
 from transformers import AutoTokenizer
 
 # # TODO: Link to Eleuther's custom default tokenizer when ready.
-DEFAULT_SPM_PATH = "gs://t5-data/vocabs/cc_all.32000.100extra/sentencepiece.model"  # GCS
-# DEFAULT_SPM_PATH = "gs://improved-t5/vocabs/tokenizer.model"  # Final model tokenizer
+# DEFAULT_SPM_PATH = "gs://t5-data/vocabs/cc_all.32000.100extra/sentencepiece.model"  # GCS
+DEFAULT_SPM_PATH = "gs://improved-t5/vocabs/tokenizer.model"  # Final model tokenizer
 
 def get_default_vocabulary():
     return seqio.SentencePieceVocabulary(DEFAULT_SPM_PATH)
