17c7e32fe3d114aa431343693b68cb9c2a63a0c7
lintangsutawika <lintang@sutawika.com>
2023-05-06 07:01:38 +0000

    25% causal

--- experiments/improved_t5/pretrain_vanilla_large.sh
+++ experiments/improved_t5/pretrain_vanilla_large.sh
@@ -5,7 +5,7 @@ python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/large.gin" \
     --gin_file="configs/task/pretrain/pile_mixed_objective.gin" \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
-    --gin.MIXTURE_OR_TASK_NAME=\""pile_ul2_causal_0_50"\" \
+    --gin.MIXTURE_OR_TASK_NAME=\""pile_ul2_causal_0_25"\" \
     --gin.TASK_FEATURE_LENGTHS="{'inputs':2048, 'targets': 2048}" \
     --gin.BATCH_SIZE=512 \
     --gin.TRAIN_STEPS=1000000 \
