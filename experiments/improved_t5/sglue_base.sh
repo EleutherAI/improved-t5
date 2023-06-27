d99a122938cb3ecde06955cbc3db144702aaa3a2
lintangsutawika <lintang@sutawika.com>
2023-06-27 10:53:37 +0000

    edit bucket

--- experiments/improved_t5/sglue_base.sh
+++ experiments/improved_t5/sglue_base.sh
@@ -7,9 +7,9 @@ python -m t5x.train \
     --gin_file="configs/task/finetune/sglue.gin" \
     --gin.TRAIN_STEPS=1_128_000 \
     --gin.SAVING_PERIOD=5000 \
-    --gin.MODEL_DIR=\"${MODEL_DIR}'sglue_finetune'\" \
+    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/t5v2/vanilla_base/checkpoint_1000000/sglue_finetune\" \
     --gin.INITIAL_CHECKPOINT_PATH=\"gs://improved-t5/ckpts/t5v2/vanilla_base/checkpoint_1000000\" \
-    --seqio_additional_cache_dirs="${CACHED_DATA_DIR}" \
+    --seqio_additional_cache_dirs=\"gs://improved-t5/data\" \
     --alsologtostderr
     # --multiprocess_gpu \
     # --coordinator_address=${ADDR} \
