bd7286e549601e0d33ba607d264e5b324dfa7657
lintangsutawika <lintang@sutawika.com>
2023-06-27 10:47:02 +0000

    changes for finetuning

--- experiments/improved_t5/pretrain_large.sh
+++ experiments/improved_t5/pretrain_large.sh
@@ -2,18 +2,14 @@ ADDR=$1
 MODEL_DIR=$2
 
 python -m t5x.train \
-    --gin_file="configs/t5v2/large.gin" \
+    --gin_file="models/scalable_t5/t5_1_1/large.gin" \
     --gin_file="configs/task/pretrain/pile_mixed_objective.gin" \
-    --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""/fsx/lintangsutawika/improved_t5/tokenizer.model"\" \
+    --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.MIXTURE_OR_TASK_NAME=\""pile_ul2_causal_0_50"\" \
     --gin.TASK_FEATURE_LENGTHS="{'inputs':2048, 'targets': 2048}" \
     --gin.BATCH_SIZE=512 \
     --gin.TRAIN_STEPS=1000000 \
-    --gin.SAVING_PERIOD=1000 \
-    --gin.MODEL_DIR=\"'/fsx/lintangsutawika/improved_t5/ckpts/t5v2/large/'\" \
+    --gin.SAVING_PERIOD=10000 \
+    --gin.MODEL_DIR=\"'gs://improved-t5/ckpts/t5v2/vanilla_large/'\" \
     --gin.USE_CACHED_TASKS=False \
-    --alsologtostderr \
-    --multiprocess_gpu \
-    --coordinator_address=${ADDR} \
-    --process_count=${SLURM_NTASKS} \
-    --process_index=${SLURM_PROCID}
+    --alsologtostderr
---
+++ experiments/improved_t5/sglue_base.sh
@@ -0,0 +1,16 @@
+ADDR=$1
+MODEL_DIR=$2
+
+python -m t5x.train \
+    --gin_file="models/scalable_t5/t5_1_1/base.gin" \
+    --gin_file="configs/task/finetune/sglue.gin" \
+    --gin.TRAIN_STEPS=1_128_000 \
+    --gin.SAVING_PERIOD=5000 \
+    --gin.MODEL_DIR=\"${MODEL_DIR}'sglue_finetune'\" \
+    --gin.INITIAL_CHECKPOINT_PATH=\"gs://improved-t5/ckpts/t5v2/vanilla_base/checkpoint_1000000\" \
+    --seqio_additional_cache_dirs="${CACHED_DATA_DIR}" \
+    --alsologtostderr
+    # --multiprocess_gpu \
+    # --coordinator_address=${ADDR} \
+    # --process_count=${SLURM_NTASKS} \
+    # --process_index=${SLURM_PROCID}
