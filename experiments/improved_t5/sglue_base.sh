a84729cb1d526318dfd754c0ac22a837e1352a55
lintangsutawika <lintang@sutawika.com>
2023-06-27 10:49:43 +0000

    changed tokenizer

--- experiments/improved_t5/sglue_base.sh
+++ experiments/improved_t5/sglue_base.sh
@@ -3,6 +3,7 @@ MODEL_DIR=$2
 
 python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/base.gin" \
+    --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin_file="configs/task/finetune/sglue.gin" \
     --gin.TRAIN_STEPS=1_128_000 \
     --gin.SAVING_PERIOD=5000 \
