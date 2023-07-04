df134448a167e209f5b9725ab4fee1839da2f493
Lintang Sutawika <lintang@sutawika.com>
2023-07-04 12:59:37 +0700

    Update pretrain_large.sh

--- experiments/improved_t5/pretrain_large.sh
+++ experiments/improved_t5/pretrain_large.sh
@@ -5,6 +5,7 @@ python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/large.gin" \
     --gin_file="configs/task/pretrain/pile_mixed_objective.gin" \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
+    --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin.MIXTURE_OR_TASK_NAME=\""pile_ul2_causal_0_50"\" \
     --gin.TASK_FEATURE_LENGTHS="{'inputs':2048, 'targets': 2048}" \
     --gin.BATCH_SIZE=512 \
