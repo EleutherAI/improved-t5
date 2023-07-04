a37998c4e7f0e1360ad8c5a69ae5c6a1de8ade21
Lintang Sutawika <lintang@sutawika.com>
2023-07-04 12:59:59 +0700

    Update finetune_base_code_to_text.sh

--- experiments/improved_t5/finetune_base_code_to_text.sh
+++ experiments/improved_t5/finetune_base_code_to_text.sh
@@ -28,6 +28,7 @@ TRAIN_STEPS=$((${STEPS} * 10 + 1000000))
 python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/base.gin" \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
+    --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin_file="configs/task/finetune/codexglue/code_to_text_${CODE_LANG}.gin" \
     --gin.TRAIN_STEPS=${TRAIN_STEPS} \
     --gin.SAVING_PERIOD=${SAVING_PERIOD} \
