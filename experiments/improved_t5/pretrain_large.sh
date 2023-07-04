405a9f3d73c386fa61ea8364bef3d46d9100fe62
Lintang Sutawika <lintang@sutawika.com>
2023-07-04 12:53:53 +0700

    Update pretrain_large.sh

--- experiments/improved_t5/pretrain_large.sh
+++ experiments/improved_t5/pretrain_large.sh
@@ -10,6 +10,6 @@ python -m t5x.train \
     --gin.BATCH_SIZE=512 \
     --gin.TRAIN_STEPS=1000000 \
     --gin.SAVING_PERIOD=10000 \
-    --gin.MODEL_DIR=\"'gs://improved-t5/ckpts/t5v2/vanilla_large/'\" \
+    --gin.MODEL_DIR=\"'gs://improved-t5/ckpts/t5v2/large/'\" \
     --gin.USE_CACHED_TASKS=False \
     --alsologtostderr
