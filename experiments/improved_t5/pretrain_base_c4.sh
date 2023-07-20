240a0e89a0afa19a7e9d10bf45898645b0266b18
lintangsutawika <lintang@sutawika.com>
2023-07-20 05:29:56 +0000

    add c4 pretraining task

--- experiments/improved_t5/pretrain_base_c4.sh
+++ experiments/improved_t5/pretrain_base_c4.sh
@@ -3,8 +3,8 @@ MODEL_DIR=$2
 
 python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/base.gin" \
-    --gin_file="configs/task/pretrain/pile_mixed_objective.gin" \
-    --gin.MIXTURE_OR_TASK_NAME=\""c4_causal_0_50"\" \
+    --gin_file="configs/task/pretrain/c4_mixed_objective.gin" \
+    --gin.MIXTURE_OR_TASK_NAME=\""c4_ul2_causal_0_50"\" \
     --gin.TASK_FEATURE_LENGTHS="{'inputs':2048, 'targets': 2048}" \
     --gin.BATCH_SIZE=512 \
     --gin.TRAIN_STEPS=1000000 \
