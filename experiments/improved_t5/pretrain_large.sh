9fe6e81186ba8ff37b8ae138ae33e660c188a1e1
lintangsutawika <lintang@sutawika.com>
2023-07-20 08:36:02 +0000

    update training params

--- experiments/improved_t5/pretrain_large.sh
+++ experiments/improved_t5/pretrain_large.sh
@@ -7,10 +7,8 @@ python -m t5x.train \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin.MIXTURE_OR_TASK_NAME=\""pile_ul2_causal_0_50"\" \
-    --gin.TASK_FEATURE_LENGTHS="{'inputs':2048, 'targets': 2048}" \
-    --gin.BATCH_SIZE=512 \
     --gin.TRAIN_STEPS=1000000 \
     --gin.SAVING_PERIOD=10000 \
-    --gin.MODEL_DIR=\"'gs://improved-t5/ckpts/t5v2/large/'\" \
+    --gin.MODEL_DIR=\"'gs://improved-t5/ckpts/v2_large/'\" \
     --gin.USE_CACHED_TASKS=False \
     --alsologtostderr
