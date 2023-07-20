1c6e6f98e9208d501a5bcc56114691c8a0f23ec1
lintangsutawika <lintang@sutawika.com>
2023-07-20 07:23:48 +0000

    added ori train setting

--- experiments/improved_t5/pretrain_large_t5_ori.sh
+++ experiments/improved_t5/pretrain_large_t5_ori.sh
@@ -6,6 +6,8 @@ python -m t5x.train \
     --gin_file="configs/task/pretrain/pile_mlm.gin" \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
+    --gin.TASK_FEATURE_LENGTHS="{'inputs':2048, 'targets': 2048}" \
+    --gin.BATCH_SIZE=512 \
     --gin.TRAIN_STEPS=1000000 \
     --gin.SAVING_PERIOD=100 \
     --gin.MODEL_DIR=\"'gs://improved-t5/ckpts/t5v2/large_t5_ori/'\" \
