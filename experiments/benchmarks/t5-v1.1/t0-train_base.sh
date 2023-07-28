487c9476172996d4b32f7f10b5fb8588bb69ad9c
lintangsutawika <lintang@sutawika.com>
2023-07-28 15:48:37 +0000

    benchmark scripts and change variable

--- experiments/benchmarks/t5-v1.1/t0-train_base.sh
+++ experiments/benchmarks/t5-v1.1/t0-train_base.sh
@@ -1,14 +1,10 @@
-MODEL_DIR=$1
-CACHED_DATA_DIR=$2
-USE_GPU=$3
-ADDR=$4
 
 python3 -m t5x.train \
-    --gin_file="/home/lintangsutawika/t5x/t5x/examples/t5/t5_1_1/base.gin" \
+    --gin_file="../t5x/t5x/examples/t5/t5_1_1/base.gin" \
     --gin_file="configs/task/finetune/t0_train.gin" \
     --gin.TRAIN_STEPS=1_128_000 \
     --gin.SAVING_PERIOD=4000 \
-    --gin.MODEL_DIR=\"${MODEL_DIR}'t0-train_finetune'\" \
+    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/t5_1_1_base/t0-train_finetune\" \
     --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_base/checkpoint_1000000\" \
-    --seqio_additional_cache_dirs="${CACHED_DATA_DIR}" \
+    --seqio_additional_cache_dirs=\"gs://improved-t5/data\" \
     --alsologtostderr
