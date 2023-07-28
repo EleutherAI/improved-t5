f8aa0733f9b79f4c92d98025b7c9629972ca4afa
lintangsutawika <lintang@sutawika.com>
2023-07-28 16:00:19 +0000

    sglue finetuning for xl

---
+++ experiments/improved_t5/finetune_xl_sglue.sh
@@ -0,0 +1,18 @@
+ADDR=$1
+MODEL_DIR=$2
+
+python -m t5x.train \
+    --gin_file="models/scalable_t5/t5_1_1/xl.gin" \
+    --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
+    --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
+    --gin_file="configs/task/finetune/sglue.gin" \
+    --gin.TRAIN_STEPS=1_128_000 \
+    --gin.SAVING_PERIOD=4000 \
+    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_xl/checkpoint_1000000/sglue_finetune\" \
+    --gin.INITIAL_CHECKPOINT_PATH=\"gs://improved-t5/ckpts/v2_xl/checkpoint_1000000\" \
+    --seqio_additional_cache_dirs=\"gs://improved-t5/data\" \
+    --alsologtostderr
+    # --multiprocess_gpu \
+    # --coordinator_address=${ADDR} \
+    # --process_count=${SLURM_NTASKS} \
+    # --process_index=${SLURM_PROCID}
