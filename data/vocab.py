75c14bb2d8c52ef271ef281eb11674d8e0b46d5b
lintangsutawika <lintang@sutawika.com>
2023-07-28 15:59:58 +0000

    add original t5 vocab

--- data/vocab.py
+++ data/vocab.py
@@ -3,14 +3,18 @@
 """Defines the vocabulary"""
 import seqio
 
-# DEFAULT_SPM_PATH = "gs://t5-data/vocabs/cc_all.32000.100extra/sentencepiece.model"  # GCS
-DEFAULT_SPM_PATH = "gs://improved-t5/vocabs/tokenizer.model"  # Final model tokenizer
-# DEFAULT_SPM_PATH = "/fsx/lintangsutawika/improved_t5/tokenizer.model"
+T5_SPM_PATH = "gs://t5-data/vocabs/cc_all.32000.100extra/sentencepiece.model"  # GCS
+DEFAULT_SPM_PATH = "gs://improved-t5/vocabs/tokenizer.model"  # LLAMA Tokenizer
 
 def get_default_vocabulary():
     return seqio.SentencePieceVocabulary(
         DEFAULT_SPM_PATH,
         extra_ids=100,
+        )
+
+def get_t5_vocabulary():
+    return seqio.SentencePieceVocabulary(
+        T5_SPM_PATH,
         )
 
 DEFAULT_OUTPUT_FEATURES = {
@@ -25,10 +29,14 @@ DEFAULT_OUTPUT_FEATURES = {
             add_eos=True)
 }
 
-DEFAULT_CLM_OUTPUT_FEATURES = {
-    "targets":
+T5_OUTPUT_FEATURES = {
+    "inputs": 
         seqio.Feature(
-            vocabulary=get_default_vocabulary(),
+            vocabulary=get_t5_vocabulary(),
             add_eos=True,
             required=False),
+    "targets":
+        seqio.Feature(
+            vocabulary=get_t5_vocabulary(),
+            add_eos=True)
 }
