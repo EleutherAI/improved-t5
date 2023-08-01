8d3e1866fea0a600af486b302144a8fb31dcdd5e
lintangsutawika <lintang@sutawika.com>
2023-08-01 05:25:19 +0000

    added train steps

--- experiments/improved_t5/finetune_base_sglue.sh
+++ experiments/improved_t5/finetune_base_sglue.sh
@@ -6,8 +6,8 @@ python -m t5x.train \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin_file="configs/task/finetune/sglue.gin" \
-    --gin.TRAIN_STEPS=1_128_000 \
-    --gin.SAVING_PERIOD=4000 \
+    --gin.TRAIN_STEPS=1_262_144 \
+    --gin.SAVING_PERIOD=5000 \
     --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_base/checkpoint_1000000/sglue_finetune\" \
     --gin.INITIAL_CHECKPOINT_PATH=\"gs://improved-t5/ckpts/v2_base/checkpoint_1000000\" \
     --seqio_additional_cache_dirs=\"gs://improved-t5/data\" \
