da637b8a7ad2ddd9e7726c8d147d44d94016dfd7
lintangsutawika <lintang@sutawika.com>
2023-08-08 15:08:09 +0000

    add natural prompts for superglue

---
+++ data/sglue/preprocessors.py
@@ -0,0 +1,211 @@
+import collections
+import functools
+
+from t5.data import preprocessors
+import tensorflow.compat.v2 as tf
+
+
+def natural_wsc_simple(dataset,
+               label='wsc:',
+               correct_referent_only=False):
+
+    def map_fn(x):
+        """Function to be called for every example in dataset."""
+        passage = x['text']
+        referent = x['span1_text']
+        pronoun = x['span2_text']
+        inputs = [
+            f'Passage: {passage}',
+            f'Question: In the passage above, what does the pronoun "{pronoun}" refer to? Answer: '
+        ]
+        return {
+            'inputs': tf.strings.join(inputs, separator='\n'),
+            # The reshape is necessary as otherwise the tensor has unknown rank.
+            'targets': tf.reshape(referent, shape=[]),
+            'label': x.get('label', 0),
+            'idx': x['idx'],
+        }
+
+    if correct_referent_only:
+        dataset = dataset.filter(lambda x: tf.cast(x.get('label', False), tf.bool))
+
+    return dataset.map(map_fn, num_parallel_calls=AUTOTUNE)
+
+def get_natural_text_preprocessor(builder_config):
+    """Return the glue preprocessor.
+
+    Args:
+        builder_config: a BuilderConfig
+    Returns:
+        a preprocessor function
+    """
+    if builder_config.name == "record":
+        return record
+    else:
+        benchmark_name = builder_config.name
+        if benchmark_name == "boolq":
+            label_names = ["No", "Yes"]
+        elif benchmark_name == "cb":
+            label_names = ["No", "Yes", "Maybe"]
+        elif benchmark_name == "copa":
+            label_names = builder_config.label_classes
+        elif benchmark_name == "multirc":
+            feature_names = ("question", "answer", "paragraph")
+            label_names = ["No", "Yes"]
+        elif benchmark_name == "wic":
+            feature_names = ("sentence1", "sentence2", "word")
+            label_names = ["No", "Yes"]
+        else:
+            feature_names = None
+        return functools.partial(
+            natural_super_glue,
+            benchmark_name=benchmark_name,
+            label_names=builder_config.label_classes,
+            feature_names=feature_names)
+
+@gin.configurable
+def record(dataset):
+
+    def process_answers(x):
+        """Helper fn to get one example per answer."""
+        ex = x.copy()
+        num_answers = tf.size(ex['answers'])
+
+        def duplicate_along_first_dim(t):
+        n_duplicates = tf.math.maximum(num_answers, 1)
+        return tf.broadcast_to(
+            t, shape=tf.concat([[n_duplicates], tf.shape(t)], axis=0))
+
+        for k, v in x.items():
+        if k != 'idx':
+            ex[k] = duplicate_along_first_dim(v)
+        ex['targets'] = tf.cond(
+            tf.greater(num_answers, 0), lambda: x['answers'],
+            lambda: tf.constant(['<unk>']))
+        ex['idx'] = {
+            'passage': duplicate_along_first_dim(x['idx']['passage']),
+            'query': duplicate_along_first_dim(x['idx']['query']),
+        }
+
+        return ex
+
+    def my_fn(x):
+        """Converts the processed example to text2text strings."""
+        passage = x['passage']
+        passage = tf.strings.regex_replace(passage,
+                                        r'(\.|\?|\!|\"|\')\n@highlight\n',
+                                        r'\1 ')
+        passage = tf.strings.regex_replace(passage, r'\n@highlight\n', '. ')
+        entities = tf.strings.reduce_join(x['entities'], separator=', ')
+        query = x['query']
+        strs_to_join = [
+            tf.strings.join(["Passage:", passage], separator=' '),
+            tf.strings.join(["Entities:", entities], separator=' '),
+            tf.strings.join(["Query:", query], separator=' '),
+            f'Question: What is the most likely entity to fill in @placeholder in previous query?'
+            'Answer: '
+        ]
+        joined = tf.strings.join(strs_to_join, separator='\n')
+
+        ex = {}
+
+        # Store the data index in the returned example (used by eval)
+        ex['idx/passage'] = x['idx']['passage']
+        ex['idx/query'] = x['idx']['query']
+
+        ex['inputs'] = joined
+        # Note that "answers" has been converted to a single string by the
+        # process_answers function.
+        ex['targets'] = x['targets']
+        # Pass-through full list of answers for eval
+        ex['answers'] = x['answers']
+        return ex
+
+    dataset = dataset.map(process_answers, num_parallel_calls=AUTOTUNE)
+    dataset = dataset.unbatch()
+    return dataset.map(my_fn, num_parallel_calls=AUTOTUNE)
+
+
+@seqio.map_over_dataset
+def natural_super_glue(x, benchmark_name, label_names, feature_names=None, id_key='idx'):
+
+    # If an ordering is not provided, sort feature keys to ensure a consistent
+    # order.
+    feature_keys = (
+        feature_names or sorted(set(x.keys()).difference(['label', 'idx'])))
+
+    label_name = tf.cond(
+        # When no label is provided (label == -1), use "<unk>"
+        tf.equal(x['label'], -1),
+        lambda: tf.constant('<unk>'),
+        # Otherwise grab the label text from label_names
+        lambda: tf.gather(label_names, x['label']),
+    )
+
+    ex = {}
+
+    if benchmark_name == 'multirc':
+
+        strs_to_join = [
+            tf.strings.join(["Passage: ", x['paragraph']], separator=''),
+            tf.strings.join(["Query:  ", x['question']], separator=''),
+            tf.strings.join(["Question: Is \"", x['answer'],"\" the correct answer to the query given the passage?"], separator=''),
+            "Answer: "
+            ]
+
+        # Store the data index in the returned example (used by eval)
+        ex['idx/paragraph'] = x['idx']['paragraph']
+        ex['idx/question'] = x['idx']['question']
+        ex['idx/answer'] = x['idx']['answer']
+    else:
+        # Store the data index in the returned example (used by eval)
+        if id_key:
+            ex['idx'] = x[id_key]
+
+        if benchmark_name == "boolq":
+            strs_to_join = [
+                tf.strings.join(["Passage: ", x['passage']], separator=''),
+                tf.strings.join(["Question: ", x['question'], "?"], separator=''),
+                "Answer: "
+                ]
+        elif benchmark_name == "cb":
+            strs_to_join = [
+                tf.strings.join(["Passage: ", x['premise']], separator=''),
+                tf.strings.join(["Question: ", x['hypothesis'], "?"], separator=''),
+                "Answer: "
+                ]
+        elif benchmark_name == "copa":
+            strs_to_join = [
+                tf.strings.join(["Premise:", x['premise']], separator=' '),
+                tf.strings.join(["Choice1:", x['choice1']], separator=' '),
+                tf.strings.join(["Choice2:", x['choice2']], separator=' '),
+                tf.strings.join(["Question: Between choice1 and choice2, which serves as", x['question'], "to the premise?"], separator=' '),
+                "Answer: "
+                ]
+        elif benchmark_name == "rte":
+            strs_to_join = [
+                tf.strings.join(["Passage: ", x['premise']], separator=''),
+                tf.strings.join(["Question: ", x['hypothesis'], "?"], separator=''),
+                "Answer: "
+                ]
+        elif benchmark_name == "wic":
+            strs_to_join = [
+                tf.strings.join(["Sentence 1:", x['sentence1']], separator=' '),
+                tf.strings.join(["Sentence 2:", x['sentence2']], separator=' '),
+                tf.strings.join(["Question: Is the word '", x['word'], "' used in the same way for both sentences above?"], separator=' '),
+                "Answer: "
+                ]
+
+    joined = tf.strings.join(
+        strs_to_join,
+        separator='\n')
+
+    if benchmark_name == 'multirc':
+        # Remove HTML markup.
+        joined = tf.strings.regex_replace(joined, '<br>', ' ')
+        joined = tf.strings.regex_replace(joined, '<(/)?b>', '')
+
+    ex['inputs'] = joined
+    ex['targets'] = label_name
+
+    return ex
---
+++ data/sglue/tasks_natural.py
@@ -0,0 +1,149 @@
+"""
+To cache tasks before training,
+
+seqio_cache_tasks \
+    --tasks=my_task_*,your_task \
+    --excluded_tasks=my_task_5 \
+    --output_cache_dir=/path/to/cache_dir \
+    --module_import=my.tasks \
+    --alsologtostderr
+
+For more details, see: seqio/scripts/cache_tasks_main.py
+
+"""
+
+import seqio
+import functools
+import tensorflow_datasets as tfds
+
+from t5.evaluation import metrics
+from t5.data import preprocessors
+from t5.data import postprocessors
+from t5.data.glue_utils import (
+    get_glue_postprocess_fn, 
+    get_glue_text_preprocessor, 
+    get_super_glue_metric, 
+    get_super_glue_weight_mapping, 
+    get_super_glue_weight_mapping_sentinel
+)
+
+from preprocessors import natural_wsc_simple, get_natural_text_preprocessor
+
+from data.vocab import DEFAULT_OUTPUT_FEATURES, T5_OUTPUT_FEATURES
+
+TaskRegistry = seqio.TaskRegistry
+MixtureRegistry = seqio.MixtureRegistry
+
+# ==================================== Super GLUE ======================================
+# Adapted from https://github.com/google-research/text-to-text-transfer-transformer/blob/main/t5/data/tasks.py
+# Original T5 SGLUE
+for b in tfds.text.super_glue.SuperGlue.builder_configs.values():
+    # We use a simplified version of WSC, defined below
+    if "wsc" in b.name:
+        continue
+    else:
+        glue_preprocessors = [
+            get_natural_text_preprocessor(b),
+            seqio.preprocessors.tokenize,
+            seqio.CacheDatasetPlaceholder(),
+            seqio.preprocessors.append_eos_after_trim,
+        ]
+
+    for OUTPUT_FEATURES in [DEFAULT_OUTPUT_FEATURES, T5_OUTPUT_FEATURES]:
+        if OUTPUT_FEATURES == T5_OUTPUT_FEATURES:
+            task_name = f"natural_super_glue_{b.name}_v102_t5"
+        else:
+            task_name = f"natural_super_glue_{b.name}_v102"
+
+        TaskRegistry.add(
+            task_name,
+            source=seqio.TfdsDataSource(
+                tfds_name="super_glue/%s:1.0.2" % b.name,
+                splits=["test"] if b.name in ["axb", "axg"] else None),
+            preprocessors=glue_preprocessors,
+            metric_fns=get_super_glue_metric(b.name),
+            output_features=OUTPUT_FEATURES,
+            postprocess_fn=get_glue_postprocess_fn(b))
+
+
+# ======================== Definite Pronoun Resolution =========================
+# TaskRegistry.add(
+#     "dpr_v001_simple",
+#     source=seqio.TfdsDataSource(tfds_name="definite_pronoun_resolution:1.1.0"),
+#     preprocessors=[
+#         preprocessors.definite_pronoun_resolution_simple,
+#         seqio.preprocessors.tokenize,
+#         seqio.CacheDatasetPlaceholder(),
+#         seqio.preprocessors.append_eos_after_trim,
+#     ],
+#     metric_fns=[metrics.accuracy],
+#     output_features=DEFAULT_OUTPUT_FEATURES)
+
+
+# =================================== WSC ======================================
+for OUTPUT_FEATURES in [DEFAULT_OUTPUT_FEATURES, T5_OUTPUT_FEATURES]:
+    if OUTPUT_FEATURES == T5_OUTPUT_FEATURES:
+        train_task_name = "natural_super_glue_wsc_v102_simple_train_t5"
+        eval_task_name = "natural_super_glue_wsc_v102_simple_eval_t5"
+    else:
+        train_task_name = "natural_super_glue_wsc_v102_simple_train"
+        eval_task_name = "natural_super_glue_wsc_v102_simple_eval"
+
+    TaskRegistry.add(
+        train_task_name,
+        source=seqio.TfdsDataSource(
+            tfds_name="super_glue/wsc.fixed:1.0.2", splits=["train"]),
+        preprocessors=[
+            functools.partial(natural_wsc_simple, correct_referent_only=True),
+            seqio.preprocessors.tokenize,
+            seqio.CacheDatasetPlaceholder(),
+            seqio.preprocessors.append_eos_after_trim,
+        ],
+        metric_fns=[],
+        output_features=OUTPUT_FEATURES)
+
+    TaskRegistry.add(
+        eval_task_name,
+        source=seqio.TfdsDataSource(
+            tfds_name="super_glue/wsc.fixed:1.0.2", splits=["validation", "test"]),
+        preprocessors=[
+            functools.partial(
+                natural_wsc_simple, correct_referent_only=False),
+            seqio.preprocessors.tokenize,
+            seqio.CacheDatasetPlaceholder(),
+            seqio.preprocessors.append_eos_after_trim,
+        ],
+        postprocess_fn=postprocessors.wsc_simple,
+        metric_fns=[metrics.accuracy],
+        output_features=OUTPUT_FEATURES)
+
+# These weights are based on the number of examples in each dataset.
+SUPER_GLUE_WEIGHT_MAPPING = {
+    # "dpr_v001_simple": 1_322.,
+    "natural_super_glue_wsc_v102_simple_train": 259.,
+    "natural_super_glue_wsc_v102_simple_eval": 0.,
+    "natural_super_glue_boolq_v102": 9_427.,
+    "natural_super_glue_cb_v102": 250.,
+    "natural_super_glue_copa_v102": 400.,
+    "natural_super_glue_multirc_v102": 27_243.,
+    "natural_super_glue_record_v102": 138_854.,
+    "natural_super_glue_rte_v102": 2_490.,
+    "natural_super_glue_wic_v102": 5_428.,
+    # "super_glue_axb_v102": 0.,
+    # "super_glue_axg_v102": 0.,
+}
+
+SUPER_GLUE_WEIGHT_MAPPING_T5 = {k+"_t5": v for k,v in SUPER_GLUE_WEIGHT_MAPPING.items()}
+
+_super_glue_tasks_with_weight = list(SUPER_GLUE_WEIGHT_MAPPING.items())
+_super_glue_tasks_with_weight_t5 = list(SUPER_GLUE_WEIGHT_MAPPING_T5.items())
+
+MixtureRegistry.add(
+    "natural_super_glue_v102_proportional",
+    _super_glue_tasks_with_weight
+)
+
+MixtureRegistry.add(
+    "natural_super_glue_v102_proportional_t5",
+    _super_glue_tasks_with_weight_t5
+)
