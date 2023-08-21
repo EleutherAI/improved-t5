358e43c939c3631396160b8c49b35306e0ad69c9
lintangsutawika <lintang@sutawika.com>
2023-08-21 15:35:06 +0000

    moved files to specific directories

--- experiments/improved_t5/ul2_causal/finetune_xxl_sglue.sh
+++ experiments/improved_t5/ul2_causal/finetune_xxl_sglue.sh
@@ -2,15 +2,15 @@ ADDR=$1
 MODEL_DIR=$2
 
 python -m t5x.train \
-    --gin_file="models/scalable_t5/t5_1_1/base.gin" \
+    --gin_file="models/scalable_t5/t5_1_1/xxl.gin" \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
-    --gin_file="configs/task/finetune/natural_sglue.gin" \
+    --gin_file="configs/task/finetune/sglue.gin" \
     --gin.TRAIN_STEPS=1_262_144 \
     --gin.SAVING_PERIOD=5000 \
-    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_base/checkpoint_1000000/natural_sglue_finetune\" \
-    --gin.INITIAL_CHECKPOINT_PATH=\"gs://improved-t5/ckpts/v2_base/checkpoint_1000000\" \
-    --gin.USE_CACHED_TASKS=False \
+    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_xxl/checkpoint_1000000/sglue_finetune\" \
+    --gin.INITIAL_CHECKPOINT_PATH=\"gs://improved-t5/ckpts/v2_xxl/checkpoint_1000000\" \
+    --seqio_additional_cache_dirs=\"gs://improved-t5/data\" \
     --alsologtostderr
     # --multiprocess_gpu \
     # --coordinator_address=${ADDR} \
--- experiments/improved_t5/ul2_causal/pretrain_xl.sh
+++ experiments/improved_t5/ul2_causal/pretrain_xl.sh
@@ -3,11 +3,13 @@ MODEL_DIR=$2
 
 python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/xl.gin" \
-    --gin_file="configs/task/pretrain/pile_mlm.gin" \
+    --gin_file="configs/task/pretrain/pile_mixed_objective.gin" \
+    --gin.partitioning.PjitPartitioner.model_parallel_submesh="(1, 1, 2, 1)" \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
+    --gin.MIXTURE_OR_TASK_NAME=\""pile_ul2_causal_0_50"\" \
     --gin.TRAIN_STEPS=1000000 \
-    --gin.SAVING_PERIOD=100 \
-    --gin.MODEL_DIR=\"'gs://improved-t5/ckpts/t5v2/xl_c4/'\" \
+    --gin.SAVING_PERIOD=10000 \
+    --gin.MODEL_DIR=\"'gs://improved-t5/ckpts/v2_xl/'\" \
     --gin.USE_CACHED_TASKS=False \
     --alsologtostderr
