8cc60c384bbdf42db0e56f9ba12e7d1293013a31
lintangsutawika <lintang@sutawika.com>
2023-08-21 15:13:09 +0000

    add benchmark scripts

---
+++ experiments/benchmarks/t5-v1.1/base_flan.sh
@@ -0,0 +1,18 @@
+ADDR=$1
+MODEL_DIR=$2
+
+python -m t5x.train \
+    --gin_file="../t5x/t5x/examples/t5/t5_1_1/base.gin" \
+    --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
+    --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
+    --gin_file="configs/task/finetune/flan_t5.gin" \
+    --gin.TRAIN_STEPS=1_084_000 \
+    --gin.SAVING_PERIOD=10_000 \
+    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/t5_1_1_base/flan_finetune\" \
+    --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_base/checkpoint_1000000\" \
+    --gin.USE_CACHED_TASKS=False \
+    --alsologtostderr
+    # --multiprocess_gpu \
+    # --coordinator_address=${ADDR} \
+    # --process_count=${SLURM_NTASKS} \
+    # --process_index=${SLURM_PROCID}
---
+++ experiments/benchmarks/t5-v1.1/large_flan.sh
@@ -0,0 +1,18 @@
+ADDR=$1
+MODEL_DIR=$2
+
+python -m t5x.train \
+    --gin_file="../t5x/t5x/examples/t5/t5_1_1/large.gin" \
+    --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
+    --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
+    --gin_file="configs/task/finetune/flan_t5.gin" \
+    --gin.TRAIN_STEPS=1_064_000 \
+    --gin.SAVING_PERIOD=10_000 \
+    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/t5_1_1_large/flan_finetune\" \
+    --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_large/checkpoint_1000000\" \
+    --gin.USE_CACHED_TASKS=False \
+    --alsologtostderr
+    # --multiprocess_gpu \
+    # --coordinator_address=${ADDR} \
+    # --process_count=${SLURM_NTASKS} \
+    # --process_index=${SLURM_PROCID}
