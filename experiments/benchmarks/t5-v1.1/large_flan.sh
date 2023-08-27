2ba6df3bcbb5d33364244d86f7ba882a5bf392ba
lintangsutawika <lintang@sutawika.com>
2023-08-27 02:01:14 +0000

    changed the tokenizer

--- experiments/benchmarks/t5-v1.1/large_flan.sh
+++ experiments/benchmarks/t5-v1.1/large_flan.sh
@@ -3,8 +3,6 @@ MODEL_DIR=$2
 
 python -m t5x.train \
     --gin_file="../t5x/t5x/examples/t5/t5_1_1/large.gin" \
-    --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
-    --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin_file="configs/task/finetune/flan_t5.gin" \
     --gin.TRAIN_STEPS=1_064_000 \
     --gin.SAVING_PERIOD=10_000 \
