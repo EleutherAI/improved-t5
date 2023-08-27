10a2873daff8c84df9f89669702ca2e95c5f7233
lintangsutawika <lintang@sutawika.com>
2023-08-27 15:01:35 +0000

    add partitioning arg

--- experiments/improved_t5/ul2_causal/finetune_xl_flan.sh
+++ experiments/improved_t5/ul2_causal/finetune_xl_flan.sh
@@ -3,6 +3,7 @@ MODEL_DIR=$2
 
 python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/xl.gin" \
+    --gin.partitioning.PjitPartitioner.model_parallel_submesh="(1, 1, 2, 1)" \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin_file="configs/task/finetune/flan.gin" \
