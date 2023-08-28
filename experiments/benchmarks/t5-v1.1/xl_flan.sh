a9b26ce88a7a69f6471ce4605164bea3edec17e7
lintangsutawika <lintang@sutawika.com>
2023-08-28 15:58:05 +0000

    add partitioning arg

--- experiments/benchmarks/t5-v1.1/xl_flan.sh
+++ experiments/benchmarks/t5-v1.1/xl_flan.sh
@@ -4,6 +4,7 @@ MODEL_DIR=$2
 python -m t5x.train \
     --gin_file="../t5x/t5x/examples/t5/t5_1_1/xl.gin" \
     --gin_file="configs/task/finetune/flan_t5.gin" \
+    --gin.partitioning.PjitPartitioner.model_parallel_submesh="(1, 1, 2, 1)" \
     --gin.TRAIN_STEPS=1_038_000 \
     --gin.SAVING_PERIOD=10_000 \
     --gin.MODEL_DIR=\"gs://improved-t5/ckpts/t5_1_1_xl/flan_finetune\" \
