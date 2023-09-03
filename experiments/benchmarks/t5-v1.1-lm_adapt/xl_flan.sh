d71864e64f56d037356eb16b092ac2fc17cbb830
lintangsutawika <lintang@sutawika.com>
2023-09-03 10:42:12 +0000

    add lm adapt flan finetune

---
+++ experiments/benchmarks/t5-v1.1-lm_adapt/xl_flan.sh
@@ -0,0 +1,17 @@
+ADDR=$1
+MODEL_DIR=$2
+
+python -m t5x.train \
+    --gin_file="../t5x/t5x/examples/t5/t5_1_1/xl.gin" \
+    --gin_file="configs/task/finetune/flan_t5.gin" \
+    --gin.partitioning.PjitPartitioner.model_parallel_submesh="(1, 1, 2, 1)" \
+    --gin.TRAIN_STEPS=1_038_000 \
+    --gin.SAVING_PERIOD=10_000 \
+    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/t5_1_1_lm100k_xl/flan_finetune\" \
+    --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_xl/checkpoint_1100000\" \
+    --gin.USE_CACHED_TASKS=False \
+    --alsologtostderr
+    # --multiprocess_gpu \
+    # --coordinator_address=${ADDR} \
+    # --process_count=${SLURM_NTASKS} \
+    # --process_index=${SLURM_PROCID}
