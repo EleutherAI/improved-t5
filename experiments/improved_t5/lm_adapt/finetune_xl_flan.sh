2dea17964e9a45fe759853b251cf8964fbe7f132
lintangsutawika <lintang@sutawika.com>
2023-09-03 04:56:39 +0000

    moved lm-adapt files

---
+++ experiments/improved_t5/lm_adapt/finetune_xl_flan.sh
@@ -0,0 +1,19 @@
+ADDR=$1
+MODEL_DIR=$2
+
+python -m t5x.train \
+    --gin_file="models/scalable_t5/t5_1_1/xl.gin" \
+    --gin.partitioning.PjitPartitioner.model_parallel_submesh="(1, 1, 2, 1)" \
+    --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
+    --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
+    --gin_file="configs/task/finetune/flan.gin" \
+    --gin.TRAIN_STEPS=1_038_000 \
+    --gin.SAVING_PERIOD=10_000 \
+    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_xl_mlm/checkpoint_1000000/lm_adapt_flan_finetune\" \
+    --gin.INITIAL_CHECKPOINT_PATH=\"gs://improved-t5/ckpts/v2_xl_mlm/checkpoint_1000000/lm_adapt/checkpoint_1100000\" \
+    --gin.USE_CACHED_TASKS=False \
+    --alsologtostderr
+    # --multiprocess_gpu \
+    # --coordinator_address=${ADDR} \
+    # --process_count=${SLURM_NTASKS} \
+    # --process_index=${SLURM_PROCID}
