95e573f5269f3039fbfedf6943ea0c894e3e32fa
lintangsutawika <lintang@sutawika.com>
2023-09-03 10:41:35 +0000

    add pile_prefix_lm_causal.gin

--- experiments/improved_t5/lm_adapt/s_causal/lm_adapt_base.sh
+++ experiments/improved_t5/lm_adapt/s_causal/lm_adapt_base.sh
@@ -5,7 +5,7 @@ python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/base.gin" \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
-    --gin_file="configs/task/finetune/pile_prefix_lm.gin" \
+    --gin_file="configs/task/finetune/pile_prefix_lm_causal.gin" \
     --gin.TRAIN_STEPS=1_100_000 \
     --gin.SAVING_PERIOD=10_000 \
     --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_base_mlm/checkpoint_1000000/lm_adapt_sc\" \
--- experiments/improved_t5/lm_adapt/s_causal/lm_adapt_xl.sh
+++ experiments/improved_t5/lm_adapt/s_causal/lm_adapt_xl.sh
@@ -6,7 +6,7 @@ python -m t5x.train \
     --gin.partitioning.PjitPartitioner.model_parallel_submesh="(1, 1, 2, 1)" \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
-    --gin_file="configs/task/finetune/pile_prefix_lm.gin" \
+    --gin_file="configs/task/finetune/pile_prefix_lm_causal.gin" \
     --gin.TRAIN_STEPS=1_100_000 \
     --gin.SAVING_PERIOD=10_000 \
     --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_xl_mlm/checkpoint_1000000/lm_adapt_sc\" \
