e09709f797b748723158467df7e2288c03bfc9fb
lintangsutawika <lintang@sutawika.com>
2023-09-03 05:39:57 +0000

    fixed train steps

--- experiments/improved_t5/lm_adapt/finetune_xl_flan.sh
+++ experiments/improved_t5/lm_adapt/finetune_xl_flan.sh
@@ -7,7 +7,7 @@ python -m t5x.train \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin_file="configs/task/finetune/flan.gin" \
-    --gin.TRAIN_STEPS=1_038_000 \
+    --gin.TRAIN_STEPS=1_138_000 \
     --gin.SAVING_PERIOD=10_000 \
     --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_xl_mlm/checkpoint_1000000/lm_adapt_flan_finetune\" \
     --gin.INITIAL_CHECKPOINT_PATH=\"gs://improved-t5/ckpts/v2_xl_mlm/checkpoint_1000000/lm_adapt/checkpoint_1100000\" \
