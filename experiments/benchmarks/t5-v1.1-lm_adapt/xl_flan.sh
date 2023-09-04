4b425b15108243108cab9c53a093c790a1d76510
lintangsutawika <lintang@sutawika.com>
2023-09-04 07:05:02 +0000

    add flan2021

--- experiments/benchmarks/t5-v1.1-lm_adapt/xl_flan.sh
+++ experiments/benchmarks/t5-v1.1-lm_adapt/xl_flan.sh
@@ -5,7 +5,7 @@ python -m t5x.train \
     --gin_file="../t5x/t5x/examples/t5/t5_1_1/xl.gin" \
     --gin_file="configs/task/finetune/flan_t5.gin" \
     --gin.partitioning.PjitPartitioner.model_parallel_submesh="(1, 1, 2, 1)" \
-    --gin.TRAIN_STEPS=1_038_000 \
+    --gin.TRAIN_STEPS=1_138_000 \
     --gin.SAVING_PERIOD=10_000 \
     --gin.MODEL_DIR=\"gs://improved-t5/ckpts/t5_1_1_lm100k_xl/flan_finetune\" \
     --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_xl/checkpoint_1100000\" \
--- experiments/benchmarks/t5-v1.1-lm_adapt/xl_flan2021.sh
+++ experiments/benchmarks/t5-v1.1-lm_adapt/xl_flan2021.sh
@@ -2,13 +2,13 @@ ADDR=$1
 MODEL_DIR=$2
 
 python -m t5x.train \
-    --gin_file="../t5x/t5x/examples/t5/t5_1_1/xxl.gin" \
-    --gin_file="configs/task/finetune/flan_t5.gin" \
+    --gin_file="../t5x/t5x/examples/t5/t5_1_1/xl.gin" \
+    --gin_file="configs/task/finetune/flan2021_t5.gin" \
     --gin.partitioning.PjitPartitioner.model_parallel_submesh="(1, 1, 2, 1)" \
-    --gin.TRAIN_STEPS=1_114_000 \
+    --gin.TRAIN_STEPS=1_138_000 \
     --gin.SAVING_PERIOD=10_000 \
-    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/t5_1_1_lm100k_xxl/flan_finetune\" \
-    --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_xxl/checkpoint_1100000\" \
+    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/t5_1_1_lm100k_xl/flan2021_finetune\" \
+    --gin.INITIAL_CHECKPOINT_PATH=\"gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_xl/checkpoint_1100000\" \
     --gin.USE_CACHED_TASKS=False \
     --alsologtostderr
     # --multiprocess_gpu \
