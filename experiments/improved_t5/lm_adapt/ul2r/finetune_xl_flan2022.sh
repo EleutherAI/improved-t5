9bde94d71f8554945aea94bab08e3bcd03f38d08
lintangsutawika <lintang@sutawika.com>
2023-09-06 06:34:22 +0000

    add ul2r

---
+++ experiments/improved_t5/lm_adapt/ul2r/finetune_xl_flan2022.sh
@@ -0,0 +1,20 @@
+ADDR=$1
+MODEL_DIR=$2
+
+python -m t5x.train \
+    --gin_file="models/scalable_t5/t5_1_1/xl.gin" \
+    --gin.partitioning.PjitPartitioner.model_parallel_submesh="(1, 1, 2, 1)" \
+    --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
+    --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
+    --gin_file="configs/task/finetune/flan.gin" \
+    --gin.TRAIN_STEPS=1_046_000 \
+    --gin.SAVING_PERIOD=10_000 \
+    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_xl_mlm/checkpoint_1000000/ul2r_flan2022_finetune\" \
+    --gin.INITIAL_CHECKPOINT_PATH=\"gs://improved-t5/ckpts/v2_xl_mlm/checkpoint_1000000/ul2r/checkpoint_1008000\" \
+    --seqio_additional_cache_dirs=\"gs://improved-t5/data\" \
+    --alsologtostderr
+    # --gin.USE_CACHED_TASKS=False \
+    # --multiprocess_gpu \
+    # --coordinator_address=${ADDR} \
+    # --process_count=${SLURM_NTASKS} \
+    # --process_index=${SLURM_PROCID}
---
+++ experiments/improved_t5/lm_adapt/ul2r/lm_adapt_xxl.sh
@@ -0,0 +1,15 @@
+ADDR=$1
+MODEL_DIR=$2
+
+python -m t5x.train \
+    --gin_file="models/scalable_t5/t5_1_1/xxl.gin" \
+    --gin.partitioning.PjitPartitioner.model_parallel_submesh="(1, 1, 2, 1)" \
+    --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
+    --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
+    --gin_file="configs/task/finetune/pile_ul2r.gin" \
+    --gin.TRAIN_STEPS=1_008_000 \
+    --gin.SAVING_PERIOD=10_000 \
+    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_xxl_mlm/checkpoint_1000000/ul2r\" \
+    --gin.INITIAL_CHECKPOINT_PATH=\"gs://improved-t5/ckpts/v2_xxl_mlm/checkpoint_1000000\" \
+    --gin.USE_CACHED_TASKS=False \
+    --alsologtostderr
