5ba739b036d096bb437322f9be016f855a7109a8
lintangsutawika <lintang@eleuther.ai>
2023-09-08 04:56:10 +0000

    add partition

--- experiments/improved_t5/mlm/pretrain_xxl_mlm.sh
+++ experiments/improved_t5/mlm/pretrain_xxl_mlm.sh
@@ -4,6 +4,7 @@ MODEL_DIR=$2
 python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/xxl.gin" \
     --gin_file="configs/task/pretrain/pile_mlm.gin" \
+    --gin.partitioning.PjitPartitioner.model_parallel_submesh="(1, 1, 4, 1)" \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin.TRAIN_STEPS=1000000 \
@@ -11,4 +12,3 @@ python -m t5x.train \
     --gin.MODEL_DIR=\"'gs://improved-t5/ckpts/v2_xxl_mlm/'\" \
     --gin.USE_CACHED_TASKS=False \
     --alsologtostderr
-    # --gin.partitioning.PjitPartitioner.model_parallel_submesh="(1, 1, 2, 1)" \
