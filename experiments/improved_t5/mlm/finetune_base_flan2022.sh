7d6417b5b7710e40955303b16157874b34aa1199
lintangsutawika <lintang@sutawika.com>
2023-11-11 12:34:45 +0000

    changed checkpoint source

--- experiments/improved_t5/mlm/finetune_base_flan2022.sh
+++ experiments/improved_t5/mlm/finetune_base_flan2022.sh
@@ -6,10 +6,10 @@ python -m t5x.train \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin_file="configs/task/finetune/flan2022.gin" \
-    --gin.TRAIN_STEPS=1_168_000 \
+    --gin.TRAIN_STEPS=2_084_000 \
     --gin.SAVING_PERIOD=2000 \
-    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_base_mlm/checkpoint_1000000/finetune_flan2022\" \
-    --gin.INITIAL_CHECKPOINT_PATH=\"gs://improved-t5/ckpts/v2_base_mlm/checkpoint_1000000\" \
+    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_base_mlm/checkpoint_2000000/finetune_flan2022\" \
+    --gin.INITIAL_CHECKPOINT_PATH=\"gs://improved-t5/ckpts/v2_base_mlm/checkpoint_2000000\" \
     --gin.USE_CACHED_TASKS=False \
     --alsologtostderr
     # --multiprocess_gpu \
