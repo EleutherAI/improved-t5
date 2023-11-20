e5b6c1d178f7a660d6c3b31be100491599aa335e
lintangsutawika <lintang@sutawika.com>
2023-11-20 08:43:02 +0000

    add microbatch

--- experiments/improved_t5/mlm/pretrain_xxl_mlm.sh
+++ experiments/improved_t5/mlm/pretrain_xxl_mlm.sh
@@ -8,6 +8,7 @@ python -m t5x.train \
     --gin.partitioning.standard_logical_axis_rules.parameter_partitioning_dims=2 \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
+    --gin.Trainer.num_microbatches=8 \
     --gin.TRAIN_STEPS=1000000 \
     --gin.SAVING_PERIOD=10000 \
     --gin.MODEL_DIR=\"'gs://improved-t5/ckpts/v2_xxl_mlm/'\" \
