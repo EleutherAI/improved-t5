ced322ce49c0dcc353f81c703036aa89031bdc72
lintangsutawika <lintang@sutawika.com>
2023-11-21 15:37:12 +0000

    update

--- experiments/improved_t5/mlm/pretrain_xxl_mlm.sh
+++ experiments/improved_t5/mlm/pretrain_xxl_mlm.sh
@@ -5,7 +5,7 @@ python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/xxl.gin" \
     --gin_file="configs/task/pretrain/pile_mlm.gin" \
     --gin.partitioning.standard_logical_axis_rules.activation_partitioning_dims=1 \
-    --gin.partitioning.standard_logical_axis_rules.parameter_partitioning_dims=2 \
+    --gin.partitioning.standard_logical_axis_rules.parameter_partitioning_dims=4 \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin.TRAIN_STEPS=1000000 \
