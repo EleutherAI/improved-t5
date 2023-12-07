4a0ea21db8ad4a3c89dde1ba5763d9333ae7decc
lintangsutawika <lintang@sutawika.com>
2023-12-07 08:02:07 +0000

    removed submesh

--- experiments/improved_t5/mlm/pretrain_xl_mlm.sh
+++ experiments/improved_t5/mlm/pretrain_xl_mlm.sh
@@ -4,7 +4,6 @@ MODEL_DIR=$2
 python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/xl.gin" \
     --gin_file="configs/task/pretrain/pile_mlm.gin" \
-    --gin.partitioning.PjitPartitioner.model_parallel_submesh="(1, 1, 2, 1)" \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin.TRAIN_STEPS=2000000 \
@@ -12,3 +11,4 @@ python -m t5x.train \
     --gin.MODEL_DIR=\"'gs://improved-t5/ckpts/v2_xl_mlm/'\" \
     --gin.USE_CACHED_TASKS=False \
     --alsologtostderr
+    # --gin.partitioning.PjitPartitioner.model_parallel_submesh="(1, 1, 2, 1)" \
