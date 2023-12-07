943db5deac4f7a011f00659ac1b01e8f6a1a3f15
lintangsutawika <lintang@sutawika.com>
2023-12-07 08:06:28 +0000

    add logical axis rule

--- experiments/improved_t5/mlm/pretrain_xl_mlm.sh
+++ experiments/improved_t5/mlm/pretrain_xl_mlm.sh
@@ -4,6 +4,8 @@ MODEL_DIR=$2
 python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/xl.gin" \
     --gin_file="configs/task/pretrain/pile_mlm.gin" \
+    --gin.partitioning.standard_logical_axis_rules.activation_partitioning_dims=1 \
+    --gin.partitioning.standard_logical_axis_rules.parameter_partitioning_dims=2 \
     --gin.Trainer.num_microbatches=2 \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
