a7b5e760f45c4b94c4f7e8c925f241f088449fea
lintangsutawika <lintang@sutawika.com>
2023-12-07 08:07:23 +0000

    remove logical axis rule

--- experiments/improved_t5/mlm/pretrain_xl_mlm.sh
+++ experiments/improved_t5/mlm/pretrain_xl_mlm.sh
@@ -4,9 +4,7 @@ MODEL_DIR=$2
 python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/xl.gin" \
     --gin_file="configs/task/pretrain/pile_mlm.gin" \
-    --gin.partitioning.standard_logical_axis_rules.activation_partitioning_dims=1 \
-    --gin.partitioning.standard_logical_axis_rules.parameter_partitioning_dims=2 \
-    --gin.Trainer.num_microbatches=2 \
+    --gin.partitioning.PjitPartitioner.model_parallel_submesh="(1, 1, 2, 1)" \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin.TRAIN_STEPS=2000000 \
@@ -14,4 +12,3 @@ python -m t5x.train \
     --gin.MODEL_DIR=\"'gs://improved-t5/ckpts/v2_xl_mlm/'\" \
     --gin.USE_CACHED_TASKS=False \
     --alsologtostderr
-    # --gin.partitioning.PjitPartitioner.model_parallel_submesh="(1, 1, 2, 1)" \
