e8e2254afa03b86b9f2150369905153321c857b7
lintangsutawika <lintang@sutawika.com>
2023-12-07 08:09:38 +0000

    remove num_partitions

--- experiments/improved_t5/mlm/pretrain_xl_mlm.sh
+++ experiments/improved_t5/mlm/pretrain_xl_mlm.sh
@@ -4,7 +4,6 @@ MODEL_DIR=$2
 python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/xl.gin" \
     --gin_file="configs/task/pretrain/pile_mlm.gin" \
-    --gin.partitioning.PjitPartitioner.num_partitions=2 \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin.TRAIN_STEPS=2000000 \
