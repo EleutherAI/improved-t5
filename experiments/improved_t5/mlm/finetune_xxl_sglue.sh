0f54269e01b2a3ced4fa922216feb0e9cf1f05ab
lintangsutawika <lintang@sutawika.com>
2023-12-27 09:19:18 +0000

    add partition

--- experiments/improved_t5/mlm/finetune_xxl_sglue.sh
+++ experiments/improved_t5/mlm/finetune_xxl_sglue.sh
@@ -7,7 +7,8 @@ python -m t5x.train \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin_file="configs/task/finetune/sglue.gin" \
     --gin.TRAIN_STEPS=1_262_144 \
-    --gin.Trainer.num_microbatches=2 \
+    --gin.partitioning.standard_logical_axis_rules.activation_partitioning_dims=1 \
+    --gin.partitioning.standard_logical_axis_rules.parameter_partitioning_dims=2 \
     --gin.SAVING_PERIOD=5000 \
     --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_xxl_mlm/checkpoint_1000000/sglue_finetune\" \
     --gin.INITIAL_CHECKPOINT_PATH=\"gs://improved-t5/ckpts/v2_xxl_mlm/checkpoint_1000000\" \
