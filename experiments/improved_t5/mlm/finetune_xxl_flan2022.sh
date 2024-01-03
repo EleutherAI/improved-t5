c08c363012e0d0e320fc94a29adf370b81b0347a
lintangsutawika <lintang@sutawika.com>
2024-01-03 08:13:44 +0000

    revert back

--- experiments/improved_t5/mlm/finetune_xxl_flan2022.sh
+++ experiments/improved_t5/mlm/finetune_xxl_flan2022.sh
@@ -6,9 +6,7 @@ python -m t5x.train \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin_file="configs/task/finetune/flan2022.gin" \
-    --gin.partitioning.standard_logical_axis_rules.activation_partitioning_dims=1 \
-    --gin.partitioning.standard_logical_axis_rules.parameter_partitioning_dims=2 \
-    --gin.TRAIN_STEPS=1_028_000 \
+    --gin.TRAIN_STEPS=1_014_000 \
     --gin.SAVING_PERIOD=2000 \
     --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_xxl_mlm/checkpoint_1000000/finetune_flan2022\" \
     --gin.INITIAL_CHECKPOINT_PATH=\"gs://improved-t5/ckpts/v2_xxl_mlm/checkpoint_1000000\" \
