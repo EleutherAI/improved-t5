ce69714159a643080ae387a473e73db396a86b56
lintangsutawika <lintang@sutawika.com>
2024-01-03 07:04:44 +0000

    partition

--- experiments/improved_t5/mlm/finetune_xxl_code_to_text.sh
+++ experiments/improved_t5/mlm/finetune_xxl_code_to_text.sh
@@ -29,9 +29,10 @@ python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/xxl.gin" \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
-    --gin.partitioning.standard_logical_axis_rules.activation_partitioning_dims=1 \
+    --gin.partitioning.standard_logical_axis_rules.activation_partitioning_dims=2 \
     --gin.partitioning.standard_logical_axis_rules.parameter_partitioning_dims=2 \
     --gin_file="configs/task/finetune/codexglue/code_to_text_${CODE_LANG}.gin" \
+    --gin.train.concurrent_metrics=False \
     --gin.TRAIN_STEPS=${TRAIN_STEPS} \
     --gin.SAVING_PERIOD=${SAVING_PERIOD} \
     --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_xxl_mlm/checkpoint_1000000/codexglue_${CODE_LANG}_finetune\" \
