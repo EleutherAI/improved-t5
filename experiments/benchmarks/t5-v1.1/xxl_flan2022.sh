ee2ecb8cb690decf1b069a2d7dc518e85402d68d
lintangsutawika <lintang@sutawika.com>
2024-01-06 14:36:07 +0000

    add microbatch

--- experiments/benchmarks/t5-v1.1/xxl_flan2022.sh
+++ experiments/benchmarks/t5-v1.1/xxl_flan2022.sh
@@ -6,7 +6,7 @@ python -m t5x.train \
     --gin_file="configs/task/finetune/flan2022_t5.gin" \
     --gin.partitioning.standard_logical_axis_rules.activation_partitioning_dims=1 \
     --gin.partitioning.standard_logical_axis_rules.parameter_partitioning_dims=2 \
-    --gin.trainer.Trainer.num_microbatches=4 \
+    --gin.trainer.Trainer.num_microbatches=8 \
     --gin.TRAIN_STEPS=1_014_000 \
     --gin.SAVING_PERIOD=2000 \
     --gin.MODEL_DIR=\"gs://improved-t5/ckpts/t5_1_1_xxl/finetune_flan2022\" \
