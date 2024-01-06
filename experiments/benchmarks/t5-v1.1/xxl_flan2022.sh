362ed3d108c4b4153d6e6f889299cc9d2b70e31a
lintangsutawika <lintang@sutawika.com>
2024-01-06 16:10:12 +0000

    remove microbatch

--- experiments/benchmarks/t5-v1.1/xxl_flan2022.sh
+++ experiments/benchmarks/t5-v1.1/xxl_flan2022.sh
@@ -5,7 +5,7 @@ python -m t5x.train \
     --gin_file="../t5x/t5x/examples/t5/t5_1_1/xxl.gin" \
     --gin_file="configs/task/finetune/flan2022_t5.gin" \
     --gin.partitioning.standard_logical_axis_rules.activation_partitioning_dims=1 \
-    --gin.partitioning.standard_logical_axis_rules.parameter_partitioning_dims=2 \
+    --gin.partitioning.standard_logical_axis_rules.parameter_partitioning_dims=4 \
     --gin.TRAIN_STEPS=1_014_000 \
     --gin.SAVING_PERIOD=2000 \
     --gin.MODEL_DIR=\"gs://improved-t5/ckpts/t5_1_1_xxl/finetune_flan2022\" \
