f4e1db49d640ca6f43a528335a3c082aaf817dbc
lintangsutawika <lintang@sutawika.com>
2024-01-17 09:05:21 +0000

    fixed steps

--- experiments/improved_t5/mlm/extend_base.sh
+++ experiments/improved_t5/mlm/extend_base.sh
@@ -6,7 +6,7 @@ python -m t5x.train \
     --gin_file="configs/task/finetune/extend_2048.gin" \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
-    --gin.TRAIN_STEPS=1000 \
+    --gin.TRAIN_STEPS=1001000 \
     --gin.SAVING_PERIOD=1000 \
     --gin.INITIAL_CHECKPOINT_PATH=\"'gs://improved-t5/ckpts/v2_base_mlm/checkpoint_1000000'\" \
     --gin.MODEL_DIR=\"'gs://improved-t5/ckpts/v2_base_mlm/_extend_2048'\" \
--- experiments/improved_t5/mlm/extend_xxl.sh
+++ experiments/improved_t5/mlm/extend_xxl.sh
@@ -8,7 +8,7 @@ python -m t5x.train \
     --gin.partitioning.standard_logical_axis_rules.parameter_partitioning_dims=2 \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
-    --gin.TRAIN_STEPS=1000 \
+    --gin.TRAIN_STEPS=1001000 \
     --gin.SAVING_PERIOD=1000 \
     --gin.INITIAL_CHECKPOINT_PATH=\"'gs://improved-t5/ckpts/v2_xxl_mlm/checkpoint_1000000'\" \
     --gin.MODEL_DIR=\"'gs://improved-t5/ckpts/v2_xxl_mlm/_extend_2048'\" \
