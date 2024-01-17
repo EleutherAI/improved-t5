02dd5d656a4b1cfe6aa553acbc002640db7fc826
lintangsutawika <lintang@sutawika.com>
2024-01-17 17:02:49 +0000

    add partition

--- experiments/improved_t5/mlm/extend_xl.sh
+++ experiments/improved_t5/mlm/extend_xl.sh
@@ -4,6 +4,8 @@ MODEL_DIR=$2
 python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/xl.gin" \
     --gin_file="configs/task/finetune/extend_2048.gin" \
+    --gin.partitioning.standard_logical_axis_rules.activation_partitioning_dims=1 \
+    --gin.partitioning.standard_logical_axis_rules.parameter_partitioning_dims=2 \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin.TRAIN_STEPS=1001000 \
