7c5733cf03de04b27da203e1402fe1c1d19e0ff5
lintangsutawika <lintang@sutawika.com>
2024-01-18 09:06:41 +0000

    changed parameters

--- experiments/improved_t5/mlm/finetune_xl_flan2022.sh
+++ experiments/improved_t5/mlm/finetune_xl_flan2022.sh
@@ -7,9 +7,9 @@ python -m t5x.train \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin_file="configs/task/finetune/flan2022.gin" \
-    --gin.TRAIN_STEPS=1_076_000 \
+    --gin.TRAIN_STEPS=1_038_000 \
     --gin.SAVING_PERIOD=2000 \
-    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_xl_mlm/checkpoint_1000000/finetune_flan2022\" \
+    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_xl_mlm/_finetune/checkpoint_1000000/flan2022_finetune\" \
     --gin.INITIAL_CHECKPOINT_PATH=\"gs://improved-t5/ckpts/v2_xl_mlm/checkpoint_1000000\" \
     --gin.USE_CACHED_TASKS=False \
     --alsologtostderr
