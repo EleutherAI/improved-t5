e46a125d68ed4437327ea71ec3daed9e94ea8ebd
lintangsutawika <lintang@sutawika.com>
2024-01-18 02:53:08 +0000

    add USE_CACHED_TASKS

--- experiments/improved_t5/mlm/extend_large.sh
+++ experiments/improved_t5/mlm/extend_large.sh
@@ -11,4 +11,5 @@ python -m t5x.train \
     --gin.INITIAL_CHECKPOINT_PATH=\"'gs://improved-t5/ckpts/v2_large_mlm/checkpoint_1000000'\" \
     --gin.MODEL_DIR=\"'gs://improved-t5/ckpts/v2_large_mlm/_extend_2048'\" \
     --gin.Trainer.num_microbatches=2 \
+    --gin.USE_CACHED_TASKS=False \
     --alsologtostderr
