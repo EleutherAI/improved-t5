8cf6b57c21e158508df9d9088f8be4fd84a954cc
lintangsutawika <lintang@sutawika.com>
2024-01-18 13:04:03 +0000

    add option to set training step start

--- experiments/improved_t5/mlm/finetune_large_flan2022.sh
+++ experiments/improved_t5/mlm/finetune_large_flan2022.sh
@@ -1,15 +1,16 @@
-ADDR=$1
-MODEL_DIR=$2
+# ADDR=$1
+# MODEL_DIR=$2
+TRAIN_STEPS=$(( ${1} + 64000 ))
 
 python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/large.gin" \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""gs://improved-t5/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin_file="configs/task/finetune/flan2022.gin" \
-    --gin.TRAIN_STEPS=1_064_000 \
+    --gin.TRAIN_STEPS=$TRAIN_STEPS \
     --gin.SAVING_PERIOD=2000 \
-    --gin.MODEL_DIR=\"gs://improved-t5/ckpts/v2_large_mlm/_finetune/checkpoint_1000000/flan2022_finetune\" \
-    --gin.INITIAL_CHECKPOINT_PATH=\"gs://improved-t5/ckpts/v2_large_mlm/checkpoint_1000000\" \
+    --gin.MODEL_DIR=\""gs://improved-t5/ckpts/v2_large_mlm/_finetune/checkpoint_${STEPS}/flan2022_finetune"\" \
+    --gin.INITIAL_CHECKPOINT_PATH=\""gs://improved-t5/ckpts/v2_large_mlm/checkpoint_${STEPS}"\" \
     --gin.USE_CACHED_TASKS=False \
     --alsologtostderr
     # --multiprocess_gpu \
