2452cea92a911192b60b584a6fef5f869dc2306b
lintang <lintang@eleuther.ai>
2024-04-02 05:54:53 +0000

    update

--- convert_weights/upload.sh
+++ convert_weights/upload.sh
@@ -1,34 +1,19 @@
 SIZE=$1
-START=$2
-END=$3
-GCP_BUCKET="gs://improved-t5/ckpts/v2_${SIZE}_mlm"
-CHECKPOINT_PATH="/weka/lintangsutawika/01-t5v2/ckpts"
-
-HF_PATH="${CHECKPOINT_PATH}/hf/t5-v2-${SIZE}"
+HF_MODEL_PATH=$2
+HF_PATH=$3
+T5X_PATH=$4
 
 mkdir -p "${HF_PATH}"
-git clone https://huggingface.co/EleutherAI/t5-v2-${SIZE} "${HF_PATH}"
-git -C "${HF_PATH}" remote set-url origin "https://lintang:${HF_KEY}@huggingface.co/EleutherAI/t5-v2-${SIZE}"
 
-for STEP in $(eval echo "{$START..$END..10000}")
-do
-    T5X_PATH="${CHECKPOINT_PATH}/t5x/t5-v2-${SIZE}-step-${STEP}"
-    mkdir -p "${T5X_PATH}"
+git clone "https://huggingface.co/${HF_MODEL_PATH}" "${HF_PATH}"
+git -C "${HF_PATH}" remote set-url origin "https://${HF_USERNAME}:${HF_KEY}@huggingface.co/${HF_MODEL_PATH}"
 
-    # Switch branch
-    git -C "${HF_PATH}" checkout -b "step_${STEP}"
-    git -C "${HF_PATH}" config http.postBuffer 524288000
-    # Download from GCP
-    gsutil -m cp -r "${GCP_BUCKET}/checkpoint_${STEP}/*" $T5X_PATH
-    bash scripts/convert_v2.sh ${SIZE} $T5X_PATH $HF_PATH
+# in main branch
+git -C "${HF_PATH}" checkout main
+git -C "${HF_PATH}" config http.postBuffer 524288000
 
-    git -C "${HF_PATH}" add .
-    git -C "${HF_PATH}" commit -am "add files for step ${STEP}"
-    git -C "${HF_PATH}" push origin "step_${STEP}"
+bash scripts/convert_v2.sh ${SIZE} $T5X_PATH $HF_PATH
 
-    # Delete Branch
-    git -C "${HF_PATH}" checkout main
-    git -C "${HF_PATH}" branch -D "step_${STEP}"
-
-    rm -rf "${T5X_PATH}"
-done
+git -C "${HF_PATH}" add .
+git -C "${HF_PATH}" commit -am "add files"
+git -C "${HF_PATH}" push origin main
