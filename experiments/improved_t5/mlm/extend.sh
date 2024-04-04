189076ef3de3f7d50082b59834c7ec321682f388
lintang <lintang@eleuther.ai>
2024-04-04 00:42:04 +0000

    causal

--- experiments/improved_t5/mlm/extend.sh
+++ experiments/improved_t5/mlm/extend.sh
@@ -5,6 +5,7 @@ EXTEND_FROM=$4
 EXTEND_TO=$5
 NUM_PARTITIONS=$6
 NUM_MICROBATCHES=$7
+CAUSAL=$8
 
 if [[ $NUM_PARTITIONS == "" ]]; then
     NUM_PARTITIONS=2
@@ -16,7 +17,7 @@ fi
 
 python -m t5x.train \
     --gin_file="models/scalable_t5/t5_1_1/${SIZE}.gin" \
-    --gin_file="configs/task/finetune/extend_${EXTENSION}.gin" \
+    --gin_file="configs/task/finetune/extend_${EXTENSION}${CAUSAL}.gin" \
     --gin.seqio.SentencePieceVocabulary.sentencepiece_model_file=\""${GCP_BUCKET}/vocabs/tokenizer.model"\" \
     --gin.seqio.SentencePieceVocabulary.extra_ids=100 \
     --gin.TRAIN_STEPS=${TRAIN_STEPS} \
